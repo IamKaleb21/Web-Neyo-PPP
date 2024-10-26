f222f145396b01602afabeb903eab362f273dc4f
Carlos Aguilar <prueba@correo.com>
2024-10-26 17:59:37 -0500

    Inicio

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,13 +5,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "Productos111"}
-
-@app.get("/productos")
-def get_products():
-    return {"productos": "platano"}
-
-
-@app.get("/productos/capy")
-def get_products():
-    return {"productos": "capy"}
-    return {"productos": "pera"}
